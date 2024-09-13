137995042ca2b3efb657c3199bfd3e6f1d35f07c
Samuel <118735723+SamuelDelabranche@users.noreply.github.com>
2024-09-13 22:28:15 +0200

    Add project files.

---
+++ ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,21 @@
+﻿using LittleUserManager.Models;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LittleUserManager.ViewModels
+{
+    public class MainWindowViewModel : Observable
+    {
+        public ObservableCollection<UserViewModel> Users { get; set; }
+
+        public MainWindowViewModel()
+        {
+            Users = new ObservableCollection<UserViewModel>();
+            Users.Add(new UserViewModel(new User("Samuel", "samuel.delabranche0@gmail.com", new DateTime(2004,12,04))));
+        }
+    }
+}
---
+++ ViewModels/UserViewModel.cs
@@ -0,0 +1,24 @@
+﻿using LittleUserManager.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LittleUserManager.ViewModels
+{
+    public class UserViewModel
+    {
+        public string Name { get; }
+        public string Email { get; }
+        public string BirthDate { get; }
+
+        public UserViewModel(User user)
+        {
+            Name = user.Name;
+            Email = user.Email;
+            BirthDate = user.BirthDate.ToString("D");
+        }
+
+    }
+}
